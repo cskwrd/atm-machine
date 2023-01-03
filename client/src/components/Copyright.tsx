6836afb7a9b056574b8aa7d055213c3c4cdf64c5
cskwrd <cskwrd@users.noreply.github.com>
2023-01-03 17:26:27 -0500

    Convert copyright page

--- client/src/components/Copyright.tsx
+++ client/src/components/Copyright.tsx
@@ -1,4 +1,4 @@
-import { Link, styled, Typography } from "@mui/material";
+import { Link, Typography } from "@mui/material";
 import FavoriteIcon from '@mui/icons-material/Favorite';
 
 
@@ -8,7 +8,7 @@ export default function Copyright() {
     <Typography mt="-2" variant="body2" align="center" sx={{ color: 'text.secondary'}}>
             &copy; ATM Machine | Open Source &nbsp; 
             <FavoriteIcon  color="error"  sx={{ fontSize: 18 ,mb:'-2px', ml:'-5px' }}/><br/>
-            <Link variant="subtitle3" component={'a'} href="https://github.com/cskwrd/atm-machine" target="_blank">
+            <Link variant="subtitle2" component={'a'} href="https://github.com/cskwrd/atm-machine" target="_blank">
             [GitHub]
             </Link>
           </Typography>
